70447a6d1c18a30f5a8ae3d6fb3390e9e3c62390
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-01-15 21:28:28 +0100

    Home page and mobile project page

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,12 +1,13 @@
 function toggleMenu() {
-var x = document.getElementById("toggle-menu");
-  if (x.className === "nav-menu-links") {
-      x.className += " show-menu";
-      document.getElementById("hamburg-menu").style.visibility = "hidden";
-      document.getElementById("close-hamburg-menu").style.visibility = "visible";
+var element = document.getElementById("toggle-menu");
+  if (element.className === "nav-menu-container") {
+      // gör navigation menu visible genom att lägga till en ny klass
+      element.className += " show-menu";
+      document.getElementById("hamburg-menu").style.display = "none";
+      document.getElementById("close-hamburg-menu").style.display = "block";
   } else {
-      x.className = "nav-menu-links";
-      document.getElementById("hamburg-menu").style.visibility = "visible";
-      document.getElementById("close-hamburg-menu").style.visibility = "hidden";
+      element.className = "nav-menu-container";
+      document.getElementById("hamburg-menu").style.display = "block";
+      document.getElementById("close-hamburg-menu").style.display = "none";
   }
 }
